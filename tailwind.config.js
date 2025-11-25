84877fce5dec3cb1418bd1599ca57c0375b22f7e
coindoorwallet <coindoorwallet@gmail.com>
2025-11-25 17:02:41 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  content: ["./app/**/*.{js,jsx}","./components/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#0A0A0C",
+        "muted-gray": "#B7B7C3",
+        "coin-green": "#52A447"
+      }
+    }
+  },
+  plugins: []
 };
